d0b04c9ea1db6b58a94d9c21566157a895b07984
LawranceMichaelite <40236169+LawranceMichaelite@users.noreply.github.com>
2020-05-03 15:59:48 +0530

    Update connectedComponentsDFS.cpp

--- Graphs/connectedComponentsDFS.cpp
+++ Graphs/connectedComponentsDFS.cpp
@@ -1,65 +1,55 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int noOfNode = 13;
-int components[noOfNode] ;
-
-void dfs(int currentNode ,int graph[noOfNode][noOfNode], bool *visited, int count)
-{
-    if(visited[currentNode] != true)
-    {
-        visited[currentNode] = true;
-        components[currentNode] = count ;
-        int *neighbours = *(graph+currentNode);
-        for(int i = 0 ; i < noOfNode; i++)
-        {
-            if(neighbours[i] != 0)
-            {
-                dfs( i , graph , visited, count );
-            }
-        }
-    }
+void addEdge(int u , int v, list<int>*adj) {
+	adj[u].push_back(v);
+	adj[v].push_back(u);
 }
 
-
-int noOfConnectedComponents(int graph[noOfNode][noOfNode])
-{
-    bool *visited = new bool[noOfNode];
-    for(int i = 0 ; i < noOfNode ; i++)
-    {
-        visited[i] = false ;
-    }
-    int count = 0 ;
-    for(int i = 0 ; i < noOfNode ; i++)
-    {
-        if( visited[i] != true )
-        {
-            dfs(i , graph , visited, count );
-            count++;        
-        }
-    }
-    return count ;
+void dfs(list<int>*adj,int count,int *connected,bool *visited ,int node) {
+	if(!visited[node]) {
+		connected[node] = count;
+		visited[node] = true;
+		for(auto i = adj[node].begin() ; i != adj[node].end() ; i++) {
+			if(!visited[*i]) {
+				dfs(adj,count,connected,visited,*i);
+			}
+		}
+	}
 }
 
-int main()
-{                                     //1   2   3   4   5   6   7   8   9   10  11  12  13 
-    int graph[noOfNode][noOfNode] = { { 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 1 , 0 , 0 , 0  },
-                                      { 0 , 0 , 1 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0  },
-                                      { 0 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0  },
-                                      { 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0  },
-                                      { 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0  },
-                                      { 0 , 1 , 1 , 1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0  },
-                                      { 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0  },
-                                      { 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 1 , 0 , 0 , 0 , 0  },
-                                      { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 1 , 0 , 0 , 0  },
-                                      { 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 , 0  },
-                                      { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0  },
-                                      { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 1  },
-                                      { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0  }
-                                    };
-    cout << noOfConnectedComponents(graph) << endl;
-    for(int i = 0 ; i < noOfNode ; i++)
-        cout << components[i] << " ";
-    return 0 ;
+int numberOfConnectedComponents(int *connected, list<int>*adj,int noOfNode) {
+	int count = 0;
+	bool visited[noOfNode];
+	for(int i = 0 ; i < noOfNode ; i++) {
+		visited[i] = false;
+	}
+	for(int i = 0 ; i < noOfNode ; i++) {
+		if(!visited[i]) {
+			dfs(adj,count,connected,visited,i);
+			count++;
+		}
+	}
+	return count;
+
 }
 
+int main() {
+	freopen("output.txt" , "w" , stdout);
+	int noOfNode = 6;
+	list<int>*adj = new list<int>[noOfNode];
+	addEdge(0,1,adj);
+	addEdge(0,2,adj);
+	addEdge(1,3,adj);
+	addEdge(3,2,adj);
+	addEdge(4,5,adj);
+	addEdge(0,3,adj);
+	int *connected = new int[noOfNode];
+	for(int i = 0 ;i < noOfNode ; i++)
+		connected[i] = 0;
+	cout << numberOfConnectedComponents(connected,adj,noOfNode) << endl;
+	for(int i = 0; i < noOfNode ; i++)
+		cout << connected[i] << " ";
+	return 0;
+
+}
